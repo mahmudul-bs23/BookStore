b82fb0d32e9521cc6518e4ad34ef65b72be890e0
BS-1025 <Mahmudul.Amin@brainstation-23.com>
2023-04-17 10:06:06 +0600

    Book and Author models added & created MsSql database tables

---
+++ BookStore/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Language { get; set; }
+        public Author Author { get; set; }
+
+    }
+}
